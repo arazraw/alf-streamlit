09de82a507caff9382235e7cbc5872a5346112e4
arazraw <araz.rawshani@gmail.com>
2024-01-07 19:24:03 +0100

    added a redirect to start page at root URL

--- ALF/urls.py
+++ ALF/urls.py
@@ -1,8 +1,11 @@
 # Project's urls.py
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
+
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/home/', permanent=True)),  # Redirect root to home app
     path('admin/', admin.site.urls),
     path('home/', include('home.urls')),         # Home app
     path('dashboard/', include('dashboard.urls')),  # Dashboard app
