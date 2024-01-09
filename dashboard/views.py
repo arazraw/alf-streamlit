1bd7a3b60a29f95ecf2695bf79919d2c9553cef5
arazraw <araz.rawshani@gmail.com>
2024-01-09 01:19:44 +0100

    See readme for details. Added SwePub and new theme Nextro Able

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,12 +3,31 @@ from django.db.models import Count, Sum
 from search.models import Paper
 import plotly.express as px
 import pandas as pd
+from .forms import PaperFilterForm
 
 def dashboard(request):
+    # Handle the form submission
+    if request.method == 'POST':
+        form = PaperFilterForm(request.POST)
+        if form.is_valid():
+            start_year = form.cleaned_data['start_year']
+            end_year = form.cleaned_data['end_year']
+            if start_year and end_year:
+                papers = Paper.objects.filter(year__range=(start_year, end_year))
+            else:
+                papers = Paper.objects.all()
+    else:
+        form = PaperFilterForm()
+        papers = Paper.objects.all()
+
+    # Calculate the total number of papers and total citations
+    paper_count = papers.count()
+    total_citations = papers.aggregate(total_citations=Sum('citations'))['total_citations']
+
     # Query for the number of papers per year
-    papers_per_year_data = Paper.objects.values('year').annotate(paper_count=Count('year')).order_by('year')
+    papers_per_year_data = papers.values('year').annotate(paper_count=Count('year')).order_by('year')
     # Query for the total number of citations per year
-    citations_per_year_data = Paper.objects.values('year').annotate(total_citations=Sum('citations')).order_by('year')
+    citations_per_year_data = papers.values('year').annotate(total_citations=Sum('citations')).order_by('year')
 
     # Papers per Year Chart
     if papers_per_year_data:
@@ -27,8 +46,11 @@ def dashboard(request):
         chart_citations_div = 'No data available for citations per year'
 
     context = {
+        'form': form,
         'chart_papers_div': chart_papers_div,
-        'chart_citations_div': chart_citations_div
+        'chart_citations_div': chart_citations_div,
+        'paper_count': paper_count,
+        'total_citations': total_citations
     }
 
     return render(request, 'dashboard/dashboard.html', context)
