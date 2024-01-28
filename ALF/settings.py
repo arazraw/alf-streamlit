2026ce8182bbba21ea5615c8c2e71d988323d04b
arazraw <araz.rawshani@gmail.com>
2024-01-28 21:42:57 +0100

    added cloud database connection. currently hardcoded password, will be removed later

--- ALF/settings.py
+++ ALF/settings.py
@@ -81,13 +81,23 @@ WSGI_APPLICATION = 'ALF.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': 'aws-0-eu-central-1.pooler.supabase.com',
+        'NAME': 'postgres',
+        'USER': 'postgres.tszoiepgwjrdmbviywxz',
+        'PORT': '6543',
+        'PASSWORD': 'wBv(2+ayMwAT5_N',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
